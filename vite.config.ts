89297aa6971757e203eec9bd1e77e1718a5beb18
Arthur Korolev <asuantodilae@protonmail.com>
2023-12-02 20:39:01 +0700

    cleared homeStyles.ts and added gh-pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Space-Tourism/",
   plugins: [react()],
+  build: {
+    outDir: "dist"
+  }
 })
